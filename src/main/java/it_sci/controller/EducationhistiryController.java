8d217f6f85b3b94ca74503e845df428616713f21
Oraphin49 <pimoraphin49@gmail.com>
2023-09-01 21:52:06 +0700

    controller

--- src/main/java/it_sci/controller/EducationhistiryController.java
+++ src/main/java/it_sci/controller/EducationhistiryController.java
@@ -1,4 +1,9 @@
 package it_sci.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/personnel")
 public class EducationhistiryController {
 }
